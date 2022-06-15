42e69d0e604d6582e57a96b2090a5ab4ca1c88ef
PossumInABox <34094714+PossumInABox@users.noreply.github.com>
2022-06-15 17:30:59 +0200

    publish first version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # clean-twitter-urls
 Tampermonkey userscript to remove t.co outbound tracking links from tweets
+
+## Features
+
+1) URLS within tweets linking to an external website will be set to the actual link, not Twitter's t.co outbound trackers.
+This also applies to all links in sight within your timeline. 
+2) Also, you get an option in the left sidebar to run the script at will, for example when you've scrolled down further and fresh tweets were loaded, which have not yet been processed by this script.
+
+## Installation
+To install, copy this https://possuminabox.github.io/pr0gramm-Tagscore/pr0gramm-tagscore.user.js into your browser's address bar or simply click on it, your userscript plugin should launch and install automatically.
+
+**Heads Up**: This of course needs Tampermonkey or Greasemonkey
---
+++ cleantwitterurls.user.js
@@ -0,0 +1,98 @@
+// ==UserScript==
+// @name         Clean Twitter Outbound URLs
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Userscript to remove t.co outbound tracking links from tweets
+// @author       PossumInABox
+// @grant        none
+// @include		 https://twitter.com*
+// @downloadURL  https://possuminabox.github.io/clean-twitter-urls/cleantwitterurls.user.js
+// @updateURL    https://possuminabox.github.io/clean-twitter-urls/cleantwitterurls.user.js
+// ==/UserScript==
+
+
+(function() {
+
+// add css
+    let styles = `<style>
+#cleanLinks {
+    font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+    color: white;
+    font-size: inherit;
+}
+
+#cleanLinks:hover {
+    cursor: pointer;
+}
+
+#cleanLinks:active {
+    color: lightblue;
+}
+
+#cleanLinks a {
+    color: inherit;
+    font: inherit;
+    font-size: inherit;
+}
+
+.linkIcon {
+    width: 1.75rem;
+    font-size: x-large;
+}
+</style>`
+
+    document.head.insertAdjacentHTML("beforeend", styles)
+
+
+
+    // userscript
+    // replace t.co outbound tracking links with real url
+    const tweetSelector = '[data-testid="tweetText"]'
+    const linkSelector = 'a[target="_blank"][role="link"]'
+
+    function replaceURL(a) {
+        let actualURL = a.innerText.replace('…', '')
+        a.setAttribute("href", actualURL)
+        console.log(`${actualURL}`)
+    }
+
+    function setUrlHovers() {
+        let links = document.querySelectorAll(`${tweetSelector} ${linkSelector}`)
+        console.log(links)
+        links.forEach(a => {
+            window.replaceURL(a)
+        })
+    }
+
+    // html for button
+    let cleanButtonHTML = `<a id="cleanLinks" role="link">
+	<div class="css-1dbjc4n r-1awozwy r-sdzlij r-18u37iz r-1777fci r-dnmrzs r-xyw6el r-o7ynqc r-6416eg">
+		<div class="css-1dbjc4n linkIcon">
+			<span>&#8466;</span>
+		</div>
+		<div class="css-901oao css-bfa6kz r-vlxjld r-37j5jr r-adyw6z r-16dba41 r-135wba7 r-1joea0r r-88pszg r-bcqeeo r-qvutc0">
+			<span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">Clean Links</span>
+		</div>
+	</div>
+    </a>`
+
+    // wait for twitter to load after dom ready, injecting it immediately appears to fail
+    setTimeout(() => {
+        // make button to force run script
+        let nav = document.querySelector('nav[role="navigation"]')
+        //console.log(nav)
+        nav.innerHTML += cleanButtonHTML
+        document.getElementById("cleanLinks").addEventListener('click', setUrlHovers);
+    }, 500)
+
+
+    // wait for api to load tweet data before running replace function
+    // 2.5s appears to work, shorter times do not
+    setTimeout(() => {
+        setUrlHovers()
+    }, 2500)
+
+    console.debug("Loaded module: Clean Twitter URLs by PossumInABox | See: https://github.com/PossumInABox/clean-twitter-urls")
+
+
+})()
